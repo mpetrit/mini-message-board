623f924f79b1bbc9e9e98e262f8f8e2f73f8d38c
mpetrit <180044841+mpetrit@users.noreply.github.com>
2025-07-09 10:48:22 +0200

    Add a README and dynamic PORT

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mini-message-board
+
+Live page: https://mini-message-board-41o7.onrender.com/
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("node:path");
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const messages = [
   {
